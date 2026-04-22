c53592901741e5a911961861e9ecc45600801937
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-04-22 22:49:27 +0200

    chore: change prose to be dark compatible

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -8,7 +8,7 @@ interface MarkdownProps {
 
 export default function Markdown({ content }: MarkdownProps) {
   return (
-    <div className="prose prose-neutral max-w-none mt-4">
+    <div className="prose prose-neutral max-w-none mt-4 dark:prose-invert dark:prose-p:text-neutral-400 dark:prose-headings:text-neutral-300 dark:prose-a:text-neutral-300">
       <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
     </div>
   );
