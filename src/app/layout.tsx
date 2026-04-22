6089d6ac92a3291e53a186292f82e65b6f9083f7
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-04-22 18:29:59 +0200

    feat: add a moving background to make the page look more alive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, JetBrains_Mono, Russo_One } from "next/font/google";
 import Footer from "../components/footer";
 import "./globals.css";
+import CircuitBackground from "../components/circuit-background";
 
 const jetMono = JetBrains_Mono({
   variable: "--font-jetMono",
@@ -34,6 +35,7 @@ export default function RootLayout({
       className={`${jetMono.variable} ${russo.variable}  ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <CircuitBackground />
         <main className="mx-auto w-full max-w-4xl px-4 py-8">{children}</main>
         <Footer />
       </body>
