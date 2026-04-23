76d42aad3b7c68776ffdf82b24bf7ede82ba4729
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-04-23 09:36:32 +0200

    feat: add an about section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import { Suspense } from "react";
 import ContributionGraph from "../components/contribution-graph";
 import PostsSection from "../components/post-section";
+import Markdown from "../components/markdown";
+
+const HEADER_TEXT = `
+# Gjermund Persson Myrvang
+
+> Currently completing a Master's degree in Informatics at the University of Oslo's Department of Informatics
+
+## About Me
+
+Master's student in Informatics at the University of Oslo, with a focus on
+human-computer interaction and a soft spot for making technology feel intuitive
+and hands-on.
+
+Currently working as a teaching assistant in [IN1060](https://www.uio.no/studier/emner/matnat/ifi/IN1060/), where I help students get to grips with Arduino and physical prototyping. I also spend time at [Sonen Makerspace](https://www.mn.uio.no/ifi/tjenester/publikum/sonen/index.html) --> a creative space where curiosity and making things meet.
+
+This summer, I'm joining [SINTEF](https://www.sintef.no/en/) part-time to work on data visualization of energy systems --> turning complex model outputs into something people can actually understand and use.
+`;
 
 export default function Home() {
   return (
     <div className="max-w-2xl mx-auto px-6 py-16 flex flex-col gap-12">
-      <div className="flex flex-col gap-1">
-        <h1 className="text-4xl">Gjermund Persson Myrvang</h1>
-        <p className="text-sm text-neutral-500 pt-2 sm:p-0">
-          Currently a master student at the department of informatics at
-          University of Oslo
-        </p>
-      </div>
+      <Markdown content={HEADER_TEXT} />
 
       <Suspense
         fallback={
