98d39c195b322a982c990db3efc42647316dcdae
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-14 20:48:32 +0200

    chore: create list of apps

---
+++ src/lib/app-info.ts
@@ -0,0 +1,53 @@
+export const AppEntries = [
+  {
+    name: "Next Seven",
+    description:
+      "A simple 7-day planner designed around calm technology. It keeps your focus on what matters today and a few days ahead without overwhelming you with everything at once.",
+    appstoreLink:
+      "https://apps.apple.com/pt/app/next-seven/id6762225202?l=en-GB",
+    apkLink:
+      "https://github.com/gjermundmyrvang/THISWEEK/releases/download/v.1.0.0/nextseven.apk",
+    icon: "/apps/nextseven.png",
+    published: new Date("2026-04-23T10:17:20.000Z"),
+    isNew: true,
+  },
+  {
+    name: "UV Check",
+    description:
+      "UV check is a very simple app showing you the current UV value at your position.",
+    appstoreLink: "https://apps.apple.com/pt/app/uvcheck/id6743999293?l=en-GB",
+    apkLink:
+      "https://github.com/gjermundmyrvang/uvcheck/releases/download/v1.0.3/Application.apk",
+    icon: "/apps/uv.png",
+    published: new Date("2026-05-01T11:13:20.000Z"),
+    isNew: true,
+  },
+  {
+    name: "Airborn WX",
+    description:
+      "Made for small aircraft pilots. Fast access to METARs, SIGMETs, forecasts, webcams, turbulence, charts, and more. Plan smarter, fly safer",
+    appstoreLink: "https://apps.apple.com/no/app/airborn-wx/id6748150074?l=nb",
+    icon: "/apps/airborn.png",
+    published: new Date("2025-04-07T13:51:20.000Z"),
+    isNew: false,
+  },
+  {
+    name: "Coffee Stats",
+    description:
+      "Coffee Stats is a simple app designed to track your coffee consumption. Log every coffee you drink by selecting the type (like espresso) and adding it to your personal history.",
+    appstoreLink:
+      "https://apps.apple.com/pt/app/coffee-stats/id6744176367?l=en-GB",
+    icon: "/apps/coffee.png",
+    published: new Date("2025-04-09T08:29:20.000Z"),
+    isNew: false,
+  },
+  {
+    name: "Private Pantry",
+    description:
+      "This app is designed for simplicity. Easily store, organize, and manage your own recipes",
+    appstoreLink: "https://apps.apple.com/ng/app/private-pantry/id6747010672",
+    icon: "/apps/private.png",
+    published: new Date("2025-06-17T21:15:20.000Z"),
+    isNew: false,
+  },
+];
